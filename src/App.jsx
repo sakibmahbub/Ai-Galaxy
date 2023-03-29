4f25a97f4314918b0ed8beed56c0e61becf569a2
sakibmahbub <arafatsakib48@gmail.com>
2023-03-30 00:21:27 +0600

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header></Header>
+      <Button>Sort By Date</Button>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-center font-bold text-4xl my-4">AI Galaxy</h1>
+    </>
+  );
+};
+
+export default Header;
